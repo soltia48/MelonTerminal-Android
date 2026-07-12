56489666008fb8c2a7be585cd6e4b382717b5d7d
KIRISHIKI Yudai <yudai.kirishiki@protonmail.com>
2026-07-13 01:07:19 +0900

    update to v0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "jp.unknowntech.melonterminal"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
